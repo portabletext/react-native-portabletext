caa183aab7044586ce32bc3860dfc53cb2294a11
Ash <ashley@juice49.com>
2023-08-24 01:40:36 +0100

    feat: update Portable Text dependencies (#27)

--- src/react-native-portable-text.tsx
+++ src/react-native-portable-text.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import type {PortableTextProps} from '@portabletext/react'
 import type {TypedObject, PortableTextBlock} from '@portabletext/types'
-import {PortableText as BasePortableText, PortableTextComponentsProvider} from '@portabletext/react'
+import {PortableText as BasePortableText, mergeComponents} from '@portabletext/react'
 import {defaultComponents} from './components/defaults'
 
 export function PortableText<B extends TypedObject = PortableTextBlock>(
   props: Omit<PortableTextProps<B>, 'listNestingMode'>
 ) {
   return (
-    <PortableTextComponentsProvider components={defaultComponents}>
-      <BasePortableText {...props} />
-    </PortableTextComponentsProvider>
+    <BasePortableText
+      {...props}
+      components={mergeComponents(defaultComponents, props.components ?? {})}
+    />
   )
 }
