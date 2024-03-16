506e3755b9f61f2ef1cdf72ea282ddb1ac5b7780
Cody Olsen <stipsan@gmail.com>
2024-03-16 17:59:42 +0100

    fix: add TS exports
    Related #92

--- src/react-native-portable-text.tsx
+++ src/react-native-portable-text.tsx
@@ -4,6 +4,8 @@ import type {TypedObject, PortableTextBlock} from '@portabletext/types'
 import {PortableText as BasePortableText, mergeComponents} from '@portabletext/react'
 import {defaultComponents} from './components/defaults'
 
+export * from '@portabletext/react';
+
 export function PortableText<B extends TypedObject = PortableTextBlock>(
   props: Omit<PortableTextProps<B>, 'listNestingMode'>,
 ) {
